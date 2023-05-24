5a1ad0502af08d9b694e69ceb799e97151dd081b
David <doladepo128@gmail.com>
2023-05-24 13:37:01 +0100

    Solution to Leetcode:MissingNumber

---
+++ Sorting/Missingnumber.java
@@ -0,0 +1,32 @@
+package Sorting;
+
+import java.utils.Arrays;
+
+ class Missingnumber{
+    public static void main(String[] args){
+        int[] arr = {0, 1, 2, 3, 5, 6,}
+        //sort(arr);
+        System.out.println(missingNumber(arr));
+    }
+
+   public static int missingNumber(int[] arr){
+      int i = 0;
+      while(i < arr.length){
+        int correct = arr[i];
+        if(arr[i] < arr.length && arr[i] != arr[correct]){
+             swap(arr, i correct);
+        } else {
+           i++;    
+        }
+      } 
+          
+    }
+
+
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first]
+        arr[first] = arr[second]
+        arr[second] = temp;
+    }
+ }
