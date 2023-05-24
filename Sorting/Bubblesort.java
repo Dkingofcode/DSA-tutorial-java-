7adb14cb52750585964d000aff3c85e9504b61a5
David <doladepo128@gmail.com>
2023-05-24 08:32:44 +0100

    Added a boolean to check if the array is sorted at any value of i

--- Sorting/Bubblesort.java
+++ Sorting/Bubblesort.java
@@ -1,13 +1,19 @@
 package Sorting;
 
+import java.Util.Arrays;
+
 public class Bubblesort {
     public static void main(String[] args){
-
+       int[] arr = {3, 1, 5, 4, 2};
+       bubble(arr);
+       System.out.println(Arrays.toString(arr));
     }
 
     static void bubble(int[] arr){
+        boolean swapped;
         // run the steps n-1 times
         for (int i = 0; arr.length; i++){
+            swapped = false;
             // for each step, max item will come at the last respective index
            for(int j = 1; j < arr.length - i; j++){
              // swap if the item is smaller than the previous item
@@ -15,9 +21,14 @@ public class Bubblesort {
                // swap
                int temp = arr[j];
                arr[j] = arr[j -1];
-               arr[j -1] = temp; 
-            } 
+               arr[j -1] = temp;
+               swapped = true; 
+             } 
            } 
+           // if you did not swap for a particular value of i, it means the array ts sorted.
+           if(!swapped){
+             break;
+           }
         }
     }
 }
