fdc746d531dd4685ca888f40fc6de06e3205d339
David <doladepo128@gmail.com>
2023-06-06 10:27:04 +0100

    added the execution of the algorithms

--- Recursion/Selection.java
+++ Recursion/Selection.java
@@ -1,8 +1,12 @@
 package Recursion;
 
+import java.util.Arrays;
+
 public class Selection {
     public static void main(String[] args) {
-        System.out.println(); 
+        int[] arr = {2, 3, 4, 5, 2, 1}; 
+        selection(arr, arr.length, 0, 0);
+        System.out.println(Arrays.toString(arr)); 
     }
 
 
--- Recursion/bubble.java
+++ Recursion/bubble.java
@@ -1,8 +1,12 @@
 package Recursion;
 
+import java.util.Arrays;
+
 public class bubble {
     public static void main(String[] args) {
-        System.out.println();
+        int[] arr = {1, 3, 4, 5, 6, 3, 2};
+        bubble(arr, arr.length - 1, 0);
+        System.out.println(Arrays.toString(arr));
     }
 
     static void bubble(int[] arr, int r, int c){
