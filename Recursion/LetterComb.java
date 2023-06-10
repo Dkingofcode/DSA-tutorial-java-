231d39ea414dfbc5d42811897ee922c16cbcf1ec
David <doladepo128@gmail.com>
2023-06-10 17:34:41 +0100

    Permutation algorithm using recursion

--- Recursion/LetterComb.java
+++ Recursion/LetterComb.java
@@ -8,18 +8,18 @@ public class LetterComb {
   }   
 
 
-  // static void pad(String p, String up){
-  //   if(up.isEmpty()){
-  //       System.out.println(p);
-  //         return;
-  //   }
+  static void pad(String p, String up){
+    if(up.isEmpty()){
+        System.out.println(p);
+          return;
+    }
 
-  //   int digit = up.charAt(0) - '0'; // this will convert '2'
-  //   for(int i = (digit -1) * 3; i < digit * 3; i++ ){
-  //     char ch = (char) ('a' + i);
-  //     pad(p + ch, up.substring(1));
-  //   }
-  // }
+    int digit = up.charAt(0) - '0'; // this will convert '2'
+    for(int i = (digit -1) * 3; i < digit * 3; i++ ){
+      char ch = (char) ('a' + i);
+      pad(p + ch, up.substring(1));
+    }
+  }
 
 
   static ArrayList<String> padRet(String p, String up){
@@ -28,13 +28,34 @@ public class LetterComb {
            list.add(p);
           return list;
     }
-
+     
     int digit = up.charAt(0) - '0'; // this will convert '2'
+    
+    ArrayList<String> list = new ArrayList<>();
+
     for(int i = (digit -1) * 3; i < digit * 3; i++ ){
       char ch = (char) ('a' + i);
-      padRet(p + ch, up.substring(1));
+      list.addAll(padRet(p + ch, up.substring(1)));
     }
+    return list;
   }
+
+
+
+   static int padCount(String p, String up){
+     if (up.isEmpty()){
+        return 1;
+     }
+     int count = 0;
+     int digit = up.charAt(0) - '0'; // this will convert '2' into 
+      for (int i = (digit - 1) * 3; i < digit * 3; i++){
+          char ch = (char) ('a' + i);
+          count = count + padCount(p + ch, up.substring(1));
+      }
+      return count;
+    }     
+
+
 
 
 }
