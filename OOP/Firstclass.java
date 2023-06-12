6b755e61666c27fc77da6ddba2f44e420ccd86c0
David <doladepo128@gmail.com>
2023-06-12 18:03:12 +0100

    Started Object-Oriented-programming

---
+++ OOP/Firstclass.java
@@ -0,0 +1,21 @@
+package OOP;
+
+public class Firstclass {
+   public static void main(String[] args) {
+      int[] rno = new int[5];
+      String[] name = new String[5];
+      float[] marks  = new float[5];
+      
+      Student[] students = new Student[5];
+
+      Student david;
+   }   
+   
+   // create a class
+   // for every single student
+   class Student {
+      int rno;
+      String name;
+      float marks;
+   }
+}
