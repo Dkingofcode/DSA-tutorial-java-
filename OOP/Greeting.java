b6a37b7a24d705645a573ed4d58a3acedc1a68ea
David <doladepo128@gmail.com>
2023-06-13 20:46:13 +0100

    examples on importing pacakges and classes

---
+++ OOP/Greeting.java
@@ -0,0 +1,11 @@
+package OOP;
+
+import static OOP.Message.message;
+
+public class Greeting {
+   public static void main(String[] args) {
+      System.out.println("Hello world");
+      message();
+      
+   }    
+}
