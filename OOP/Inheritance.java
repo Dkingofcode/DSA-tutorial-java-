62c489bdbf63339c8c8f6d8d0754d399efaf2b7a
David <doladepo128@gmail.com>
2023-06-17 23:58:10 +0100

    Practicing Inheritance concepts in java

--- OOP/Inheritance.java
+++ OOP/Inheritance.java
@@ -1,9 +1,7 @@
 package OOP;
 
 public class Inheritance {
-   public static void main(String[] args) {
-    
-   }
+   
    
    public class Box {
     double l;
@@ -11,19 +9,50 @@ public class Inheritance {
      double w;
    }
 
+   private double l;
+   private double h;
+   private double w;
 
+   Inheritance() {
+      this.h = -1;
+      this.l = -1;
+      this.w = -1;
+   }
+ 
+   Inheritance(double side) {
+      // super(); Object class
 
+      this.w = side;
+      this.l = side;
+      this.h = side;
+   }
+  
+   Inheritance(double l, double h, double w){
+      System.out.println("Box class constructed");
+      this.l = l;
+      this.h = h;
+      this.w = w;
+   }
 
+   Inheritance (Box old){
+      this.h = old.h;
+      this.l = old.l;
+      this.w = old.w;
+   }
 
-   public class BoxWeight extends Box{
-     double weight;
+   public void information() {
+      System.out.println("Running the box");
+   }
 
-    //  public Boxweight(){
-    //     this.weight = -1;
-    //  }
+   // public class BoxWeight extends Box{
+   //   double weight;
 
-     public BoxWeight(double l, double h, double w,  double weight) {
-        this.weight = -1;
-     }
-   } 
+   //  //  public Boxweight(){
+   //  //     this.weight = -1;
+   //  //  }
+
+   //   public BoxWeight(double l, double h, double w,  double weight) {
+   //      this.weight = -1;
+   //   }
+  // } 
 }
